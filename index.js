2d07d851bbf1fa54119735ce15d611b90bc9f3b1
rujal454 <127338144+rujal454@users.noreply.github.com>
2023-03-10 07:03:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const str = prompt('USERNAME:');
 const str1 = prompt("PASSWORD:");
 var validRegex = /^([a-zA-Z0-9\._]+)@([[a-zA-Z0-9]+).([a-z]{2,20})$/;
         
-        if(str.match(validRegex) && str1.length>=6){
+        if(str.match(validRegex) && str1.length>=6)
+        {
             let s=str.substring(0,str.indexOf("@"));
             let u=s.toUpperCase();
-
             console.log("SUCCESSFULLY LOGGED IN "+ u+"\nUSERNAME :"+str+"\nPASSWORD :"+str1);
-            
-           
-            
-        
         }
-        else {
+        else 
+        {
             console.log("INVALID");
         }
     
